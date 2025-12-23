c9093eba1d53a09620a0e47d45c6023a5791c813
Dipesh Joshi <dipeshjoshi811@gmail.com>
2025-12-23 21:31:57 +0545

    updated the ui

--- Todo/TodoApp/static/TodoApp/js/todo.js
+++ Todo/TodoApp/static/TodoApp/js/todo.js
@@ -1,3 +1,4 @@
  function changeColor(clickedDiv) {
-    clickedDiv.style.backgroundColor = 'green'; // Change the color of the clicked div to red
+    clickedDiv.style.backgroundColor = 'green'; 
   }
+  
